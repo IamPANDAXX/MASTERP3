67c9380639015db1994e2d2968b74c364aa1a0a1
IamPANDAXX <pandax463@gmail.com>
2026-01-16 20:13:47 -0600

    esperar que parchen yt-dlp-exec, estar atento en su git

--- backend/render-build.sh
+++ backend/render-build.sh
@@ -6,4 +6,8 @@ apt-get install -y python3 python3-pip ffmpeg
 echo "Instalando yt-dlp..."
 pip install -U yt-dlp
 
+echo "Actualizando el yt-dlp..."
+pip install --upgrade yt-dlp
+
+
 echo "yt-dlp y ffmpeg instalados correctamente."
